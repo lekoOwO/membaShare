bbc346f55cc12db17290a55e64b6743460ae1b3d
Leko <rexx0520@gmail.com>
2021-02-23 01:29:35 +0800

    fix: streamlink use https-proxy

--- host/api.py
+++ host/api.py
@@ -23,7 +23,7 @@ def get_cookie():
 cookie_param = get_cookie()
 
 def get_m3u8(video_id, proxy):
-    cmd = f"streamlink --http-proxy '{proxy}' --stream-url 'https://www.youtube.com/watch?v={video_id}' {cookie_param}"
+    cmd = f"streamlink --https-proxy '{proxy}' --stream-url 'https://www.youtube.com/watch?v={video_id}' {cookie_param}"
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
 
